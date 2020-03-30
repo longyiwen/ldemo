1eca7aebe30c1edb3e9e9b7bd1b89f5175fe5d4f
86156 <86156@DESKTOP-2EP13GU>
2020-03-30 11:17:03 +0800

    新

---
+++ C0-S3-Ply-cloud-index/src/main/java/com/yc/C0S3Plycloudindex/C0S3PlyCloudIndexApplication.java
@@ -0,0 +1,13 @@
+package com.yc.C0S3Plycloudindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C0S3PlyCloudIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C0S3PlyCloudIndexApplication.class, args);
+	}
+
+}
---
+++ C0-S3-Ply-cloud-index/src/test/java/com/yc/C0S3Plycloudindex/C0S3PlyCloudIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc.C0S3Plycloudindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C0S3PlyCloudIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
